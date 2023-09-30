4386314cfc935f5ba41267285e23d8999a60b126
TWO-MAICOL <maicollopez277@outlook.com>
2023-09-30 09:31:30 -0500

    inicio de seseion universal-cookie

--- components/Home.jsx
+++ components/Home.jsx
@@ -3,25 +3,34 @@ import {Menu} from './Menu.jsx'
 import {Footer} from './Footer.jsx'
 //  view of data primeraReact 
 import React, { useState, useEffect } from "react";
-import { ProductService } from "./ProductService.jsx";
 import { Button } from "primereact/button";
 import { DataView } from "primereact/dataview";
 import { Rating } from "primereact/rating";
 import { Tag } from "primereact/tag" 
-
+// import axios para las consultas al server
+import Axios from "axios";
+import { BsCart4 } from "react-icons/bs";
+//  inicio of session
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 export const Home = ()=> {
   const [products, setProducts] = useState([]);
+ console.log(cookies.get('user'));
+  useEffect(() => { 
+    Axios.get('http://localhost:3000/getProducts')
+    .then((res) => {
+      setProducts(res.data);      
+    })    
+    .catch((err)=>{console.log(err)}) 
 
-  useEffect(() => {
-    ProductService.getProducts().then((data) => setProducts(data));
   }, []);
 
-  const getSeverity = (product) => {
-    switch (product.inventoryStatus) {
-      case 'Disponible':
+  const status = (product) => {
+    switch (product.estado) {
+      case 'disponible':
         return 'success';
-      case 'Agotado':
+      case 'agotado':
           return 'warning';      
       default:
         return null;
@@ -30,42 +39,45 @@ export const Home = ()=> {
   const itemTemplate = (product) => {
     return (
        
-      <div className="col-12">
+      <div className="col-12">  
         <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4  ">
           <img
             className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"
-            src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`}
-            alt={product.name}
+            src={`../assets/img/products/${product.routeImg}`}
+            alt={product.nombre}
           />
           <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4 border-round">
             <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-              <div className="text-2xl font-bold text-900">{product.name}</div>
-              <Rating value={product.rating} readOnly cancel={false}></Rating>
+              <div className="text-2xl font-bold text-900">{product.nombre}</div>
+              <Rating value={product.estrellas} readOnly cancel={false}></Rating>
               <div className="flex align-items-center gap-3">
                 <span className="flex align-items-center gap-2">
-                  <i className="pi pi-tag"></i>
-                  <span className="font-semibold">{product.category}</span>
+                  <i className="pi pi-tag"> </i>
+                  {/* <span className="font-semibold">{product.categoria}</span> */}
                 </span>
-                <Tag
-                  value={product.inventoryStatus}
-                  severity={getSeverity(product)}
+                <Tag 
+                  value={product.estado}
+                  severity={status(product)}
                 ></Tag>
               </div>
-            </div>
+            </div>              
             <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-              <span className="text-2xl font-semibold">${product.price}</span>
-              <Button
-                icon="pi pi-shopping-cart"
+              <span className="text-2xl font-semibold">${product.precio}</span>
+            
+              <Button              
+                icon={  <BsCart4/>}
                 className="p-button-rounded"
-                disabled={product.inventoryStatus === "OUTOFSTOCK"}
+                disabled={product.estado === "Agotado"}
               ></Button>
+              
             </div>
           </div>
         </div>
       </div>
     );
   };
-
+ 
+ 
     return (
     <>    
      <title> Home </title> 
--- components/Login.jsx
+++ components/Login.jsx
@@ -2,29 +2,43 @@ import React, { useState } from "react";
 import Axios from "axios";
 // import input con reac 
 import { InputText } from 'primereact/inputtext';
- 
+//  inicio of session
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
 export const Login = ()=> {
     const [user, setUser] = useState('');
     const [pass, setPass] = useState('');
     const [message, setmessage] = useState('');
-    
 
     const handleLogin  = async(e) => { 
       var nodeResponse = document.getElementById('message');
       e.preventDefault();     
       
       await Axios.post('http://localhost:3000/login',{name:user,password:pass})
-       .then((res)=>{      
-          nodeResponse.classList.remove('invisible');          
-          setmessage(res.data);
-          // TIME OF MESSAGE
-          setTimeout(()=>{
-            nodeResponse.classList.add('invisible');
-          },2000)
+       .then((res)=>{             
+
+        if (res.data[0].usuario ) {         
+          // variabels of session
+          var json = res.data[0];
+          cookies.set("id",json.id );
+          cookies.set("user",json.usuario);
+          cookies.set("pass", json.contrasena);     
+          window.location.href = "/home";
+
+        }else{          
+           nodeResponse.classList.remove('invisible');          
+           setmessage(res.data);
+           // TIME OF MESSAGE
+           setTimeout(()=>{
+             nodeResponse.classList.add('invisible');
+           },2000)
+
+        }
 
        })   
        .catch((err)=>{console.log(err)})
-       
+                
     }
     return (
     <>  
@@ -38,7 +52,7 @@ export const Login = ()=> {
             <div className="card z-index-0 fadeIn3 fadeInBottom">
               <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div className="bg-gradient-secondary shadow-primary border-radius-lg py-3 pe-1">
-                  <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Welcome</h4>
+                  <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Inicia Sesion</h4>
                   <div className="row mt-3">
                     <div className="col-2 text-center ms-auto">
                       <a className="btn btn-link px-3"  >
@@ -60,25 +74,26 @@ export const Login = ()=> {
               </div>
               <div className="card-body">
                 <form className="text-start">
-                        <label className="form-label">Usuario</label>                    
-                      <div className="input-group input-group-outline my-3  ">
-                        <InputText  
-                          onChange={(e) => setUser(e.target.value)}
-                          className="form-control " 
-                          placeholder="Usuario"
-                          
-                        />                                
-                      </div>                  
-                      <label className="form-label">Contraseña</label>
-                      <div className="input-group input-group-outline my-3  ">
+                    <label className="form-label">Usuario</label>                                                     
+                    <div className="input-group input-group-outline my-3  ">
+                      <InputText  
+                        onChange={(e) => setUser(e.target.value)}
+                        className="form-control " 
+                        placeholder="Usuario"
+                        
+                      />      
+                     </div>                     
+                      
+                    <label className="form-label">Contraseña</label>                 
+                    <div className="input-group input-group-outline my-3  ">
                         <InputText  
                           onChange={(e) => setPass(e.target.value)}
                           className="form-control" 
                           placeholder="Contraseña"
                           type="password"
                           
-                        />                                                                    
-                      </div>               
+                        />     
+                     </div>                                         
                      
                   <div className="text-center">
                     <button onClick={handleLogin} className="btn bg-gradient-primary w-100 my-4 mb-2">Iniciar sesion</button>
